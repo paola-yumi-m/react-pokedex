3097042e0b608db09f6dd470b9bc4c32be7d4802
paola-yumi-m <paola.yumi.m@gmail.com>
2022-12-19 09:05:33 -0300

    Testing Card component.

--- src/get-data/GetData.js
+++ src/get-data/GetData.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles.css';
-import { Card } from "../card/Card";
+import { Card } from "../Card/Card";
 
 export const GetData = ({ data, getPokemonId, getTypes }) => {
     const getPokemons = () => {
