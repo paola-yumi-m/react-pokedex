88d3465afe75f14eecb6fbce186c6d98ea62a747
paola-yumi-m <paola.yumi.m@gmail.com>
2023-08-25 12:59:59 -0300

    fixing test (id changed to pokemonId)

--- src/PokemonSelector/__test__/PokemonSelector.test.js
+++ src/PokemonSelector/__test__/PokemonSelector.test.js
@@ -1,47 +1,51 @@
-import {fireEvent, prettyDOM, render, screen} from "@testing-library/react";
-import {PokemonSelector} from "../PokemonSelector";
-import userEvent from "@testing-library/user-event";
+import { fireEvent, prettyDOM, render, screen } from '@testing-library/react';
+import { PokemonSelector } from '../PokemonSelector';
+import userEvent from '@testing-library/user-event';
 
 describe('<PokemonSelector />', function () {
-    const getPokemonIdMock = jest.fn();
-    function renderPokemonSelector() {
-        render(
-            <PokemonSelector
-                data={[
-                    {
-                        name: 'bulbasaur',
-                        id: 1
-                    },
-                    {
-                        name: 'ivysaur',
-                        id: 2
-                    }
-                ]}
-                getPokemonId={getPokemonIdMock}
-            />
-        );
-    }
+  const getPokemonIdMock = jest.fn();
+  const setSelectedPokemonMock = jest.fn();
+  function renderPokemonSelector() {
+    render(
+      <PokemonSelector
+        data={[
+          {
+            name: 'bulbasaur',
+            pokemonId: 1,
+          },
+          {
+            name: 'ivysaur',
+            pokemonId: 2,
+          },
+        ]}
+        getPokemonId={getPokemonIdMock}
+        selectedPokemon={'0'}
+        setSelectedPokemon={setSelectedPokemonMock}
+      />
+    );
+  }
 
-    it('should render pokemons in a select box with the first option being "Select pokémon"', function () {
-        renderPokemonSelector();
+  it('should render pokemons in a select box with the first option being "Select pokémon"', function () {
+    renderPokemonSelector();
 
-        const selectBox = screen.getByRole('combobox');
-        const options = selectBox.options;
+    const selectBox = screen.getByRole('combobox');
+    const options = selectBox.options;
 
-        expect(options[0].innerHTML).toBe('Select Pokémon');
-        expect(options[1].innerHTML).toBe('bulbasaur');
-        expect(options[2].innerHTML).toBe('ivysaur');
-    });
+    expect(options[0].innerHTML).toBe('Select Pokémon');
+    expect(options[1].innerHTML).toBe('bulbasaur');
+    expect(options[2].innerHTML).toBe('ivysaur');
+  });
 
-    it('should call getPokemonId with the selected pokemon id', function () {
-        renderPokemonSelector();
+  it('should call getPokemonId with the selected pokemon id', function () {
+    renderPokemonSelector();
 
-        const selectBox = screen.getByRole('combobox');
-        fireEvent.change(selectBox, {target: {value: 2 }});
-        fireEvent.change(selectBox, {target: {value: 1 }});
+    const selectBox = screen.getByRole('combobox');
+    fireEvent.change(selectBox, { target: { value: 2 } });
+    fireEvent.change(selectBox, { target: { value: 1 } });
 
-        expect(getPokemonIdMock).toHaveBeenNthCalledWith(1, "2");
-        expect(getPokemonIdMock).toHaveBeenNthCalledWith(2, "1");
-    });
-
+    expect(getPokemonIdMock).toHaveBeenNthCalledWith(1, '2');
+    expect(setSelectedPokemonMock).toHaveBeenNthCalledWith(1, '2');
+    expect(getPokemonIdMock).toHaveBeenNthCalledWith(2, '1');
+    expect(setSelectedPokemonMock).toHaveBeenNthCalledWith(2, '1');
+  });
 });
