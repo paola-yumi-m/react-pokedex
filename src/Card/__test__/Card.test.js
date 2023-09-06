141516980d83765b54e81875a3c4b6ead5ab3157
paola-yumi-m <paola.yumi.m@gmail.com>
2023-09-06 12:14:18 -0300

    fixing tests.

--- src/Card/__test__/Card.test.js
+++ src/Card/__test__/Card.test.js
@@ -1,57 +1,58 @@
-import {prettyDOM, render, screen} from "@testing-library/react";
-import {Card} from "../Card";
+import { prettyDOM, render, screen } from '@testing-library/react';
+import { Card } from '../Card';
 import '@testing-library/jest-dom';
-import userEvent from "@testing-library/user-event";
+import userEvent from '@testing-library/user-event';
 
 describe('<Card />', function () {
-    function renderCard(pokemonSrc, pokemon, getTypesMocked, getPokemonIdMock) {
-        render(
-            <Card
-                pokemonSrc={pokemonSrc}
-                pokemon={pokemon}
-                getPokemonId={getPokemonIdMock}
-                getTypes={getTypesMocked}
-            />
-        );
-    }
+  function renderCard(pokemonSrc, pokemon, getTypesMocked, getPokemonIdMock) {
+    render(
+      <Card
+        pokemonSrc={pokemonSrc}
+        pokemon={pokemon}
+        getPokemonId={getPokemonIdMock}
+        getTypes={getTypesMocked}
+      />
+    );
+  }
 
-    const getPokemonIdMock = jest.fn();
-    const getTypesMocked = jest.fn();
-    const pokemon = {
-        id: 10,
-        name: 'Caterpie'
-    };
-    const pokemonSrc = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/10.png';
+  const getPokemonIdMock = jest.fn();
+  const getTypesMocked = jest.fn();
+  const pokemon = {
+    pokemonId: 10,
+    name: 'Caterpie',
+  };
+  const pokemonSrc =
+    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/10.png';
 
-    it('should render pokemon card', function () {
-        renderCard(pokemonSrc, pokemon, getTypesMocked, getPokemonIdMock);
+  it('should render pokemon card', function () {
+    renderCard(pokemonSrc, pokemon, getTypesMocked, getPokemonIdMock);
 
-        const imgElement = screen.getByRole('img');
-        const idElement = screen.getByText('10');
-        const nameElement = screen.getByText('Caterpie');
+    const imgElement = screen.getByRole('img');
+    const idElement = screen.getByText('10');
+    const nameElement = screen.getByText('Caterpie');
 
-        expect(imgElement.src).toBe(pokemonSrc);
-        expect(idElement).toBeInTheDocument();
-        expect(nameElement).toBeInTheDocument();
-        expect(getTypesMocked).toBeCalledWith(pokemon);
-    });
+    expect(imgElement.src).toBe(pokemonSrc);
+    expect(idElement).toBeInTheDocument();
+    expect(nameElement).toBeInTheDocument();
+    expect(getTypesMocked).toBeCalledWith(pokemon);
+  });
 
-    it('should render a ? when there is no source for the image', function () {
-        renderCard(null, pokemon, getTypesMocked, getPokemonIdMock);
+  it('should render a ? when there is no source for the image', function () {
+    renderCard(null, pokemon, getTypesMocked, getPokemonIdMock);
 
-        const imgElement = screen.queryByRole('img');
-        const noImgElement = screen.getByText('?');
+    const imgElement = screen.queryByRole('img');
+    const noImgElement = screen.getByText('?');
 
-        expect(imgElement).not.toBeInTheDocument();
-        expect(noImgElement).toBeInTheDocument();
-    });
+    expect(imgElement).not.toBeInTheDocument();
+    expect(noImgElement).toBeInTheDocument();
+  });
 
-    it('should get pokémonId when card is clicked', function () {
-        renderCard(pokemonSrc, pokemon, getTypesMocked, getPokemonIdMock);
+  it('should get pokémonId when card is clicked', function () {
+    renderCard(pokemonSrc, pokemon, getTypesMocked, getPokemonIdMock);
 
-        const card = screen.getByTestId('pokemon-card');
-        userEvent.click(card);
+    const card = screen.getByTestId('pokemon-card');
+    userEvent.click(card);
 
-        expect(getPokemonIdMock).toHaveBeenCalled();
-    });
+    expect(getPokemonIdMock).toHaveBeenCalled();
+  });
 });
